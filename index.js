2c3d8d57f1d826c0c3495593d8a9bdd9b0ae531b
georgianft <chaganavageorge@gmail.com>
2020-09-23 22:14:44 +0400

    updates pets2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 class Statement {
-    constructor(title, contactPersonName, contactPersonNumber, address, description, pet, price, image, color, weight) {
+    constructor(title, contactPersonName, contactPersonNumber, address, description, pet, price, image) {
         this.image = image;
         this.title = title;
         this.contactPersonName = contactPersonName;
@@ -8,11 +8,9 @@ class Statement {
         this.description = description;
         this.pet = pet;
         this.price = price;
-        this.color = color;
-        this.weight = weight;
     }
 }
-
+ 
 class Pet {
     constructor(category, age, gender) {
         this.category = category;
@@ -20,29 +18,37 @@ class Pet {
         this.gender = gender;
     }
 }
-
+ 
 class Cat extends Pet {
     constructor(category, age, gender, toiletFriendly) {
         super(category, age, gender);
         this.toiletFriendly = toiletFriendly;
     }
 }
+ 
+const petList = document.querySelector('.pet-list');
+ 
+const statement1 = new Statement('ჩუქდება თეთრი კნუტი, 11 თვის', 'ლუკა', '599931926', 'თბილისი', 'არის ძალიან საყვარელი', null, 0, 'https://envato-shoebox-0.imgix.net/2d04/0e8a-f427-11e1-952c-842b2b692e1a/2100817-001.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=700&s=1110052f8c6d4715316ccbdd8eaad0fb');
+const statement2 = new Statement('ჩუქდება თეთრი ძაღლი, 2 თვის', 'ლუკა', '599931926', 'თბილისი', 'არის ძალიან საყვარელი', null, 0, 'https://envato-shoebox-0.imgix.net/2d04/0e8a-f427-11e1-952c-842b2b692e1a/2100817-001.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=700&s=1110052f8c6d4715316ccbdd8eaad0fb');
+const statement3 = new Statement('ჩუქდება თეთრი ზაზუნა, 3 თვის', 'ლუკა', '599931926', 'თბილისი', 'არის ძალიან საყვარელი', null, 0, 'https://envato-shoebox-0.imgix.net/2d04/0e8a-f427-11e1-952c-842b2b692e1a/2100817-001.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=700&s=1110052f8c6d4715316ccbdd8eaad0fb');
 
-class Dog extends Pet {
-    constructor(category, age, gender, smell, swim) {
-        super(category, age, gender);
-        this.smell = smell;
-        this.swim = swim;
-    }
+const array = [statement1, statement2, statement3];
+
+const appendStatement = (statement) => {
+    const statementTemplate = `
+    <div class="statement">
+                <p class="statement-title">${statement.title}</p>
+                <img class="statement-img" src=${statement.image} alt="">
+                <p class="description">${statement.description}</p>
+                <p class="author-info">ავტორის სახელი: ${statement.contactPersonName}</p>
+                <p class="author-info">საკონტაქტო ნომერი: ${statement.contactPersonNumber}</p>
+    </div>`;
+    petList.innerHTML += statementTemplate;
 }
+ 
+// appendStatement(statement1);
+// შევქმნათ განცხადებების მასივი, შევმქნათ ფუნქცია, რომელიც ციკლით გადაუყვება ამ მასივს და დახატავს ყველა განცხადებას
 
-class Hamster extends Pet {
-    constructor(category, age, gender, Rodent) {
-        super(category, age, gender);
-        this.Rodent = Rodent;
-    }
+for (let index = 0; index < array.length; index++) {
+    appendStatement(array[index]);
 }
-
-const statement = new Statement('ჩუქდება თეთრი კნუტი, 1 თვის', 'ლუკა', '599931926', 'თბილისი', 'არის ძალიან საყვარელი', null, 0, null, 'თეთრი', '500გრ');
-
-
